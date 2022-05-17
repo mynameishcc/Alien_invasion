8bd483ad00a0fdc33cb30e322f1e0a525e69348a
mynameishcc <1287674762@qq.com>
2022-05-17 23:09:10 +0800

    alien_invasion

---
+++ game_stats.py
@@ -0,0 +1,15 @@
+class GameStats:
+    def __init__(self, ai_game):
+        self.settings = ai_game.settings
+        self.reset_stats()
+        self.game_active = False
+        try:
+            with open('high_score.txt', 'r') as f:
+                self.high_score = int(f.read())
+        except FileNotFoundError:
+            self.high_score = 0
+
+    def reset_stats(self):
+        self.ships_left = self.settings.ship_limit
+        self.score = 0
+        self.level = 1
